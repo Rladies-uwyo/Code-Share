32f43e842bf1ac7d97c4fb5240614e9f8d950ec4
Molly R. Caldwell <83186124+mollyrcaldwell@users.noreply.github.com>
2023-04-10 17:22:01 -0600

    Molly's trial git practice code addition

--- Git_workshop/Practice_code/RLadies_Git_PracticeCode.R
+++ Git_workshop/Practice_code/RLadies_Git_PracticeCode.R
@@ -1,3 +1,6 @@
 #Practice code for RLadies Git Workshop (4/14/2023)
 #This code is for practicing collaborative use of Git
 #Feel free to add your own code and push to repository as an example
+
+#Molly's example code trial 4/10/2023
+a <- 2 + 2
